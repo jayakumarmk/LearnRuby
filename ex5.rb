18725813a8318e61642e857f44e3869e2c3c3147
Jayakumar <mk_jayakumar@yahoo.com>
2018-04-09 20:45:32 +0530

    Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Jayakumar'
+my_age = 48
+my_height = 178
+my_weight = 84
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about  #{my_name}."
+puts "He is #{my_height} cms tall."
+puts "He is #{my_weight} kgs heavy."
+puts "Actually should reduce another 10 kgs."
+puts "He has got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth}  depending on the coffee."
+
+puts "If i add #{my_age}, #{my_height} and #{my_weight} i get #{my_age + my_height + my_weight}. "
---
+++ ex5a.rb
@@ -0,0 +1,17 @@
+name = 'Jayakumar'
+age = 48
+height = 178
+weight = 84
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+puts "Let's talk about  #{name}."
+puts "He is #{height} cms tall."
+puts "He is #{weight} kgs heavy."
+puts "He is #{height * 0.393701} inches tall."
+puts "Actually should reduce another 10 kgs."
+puts "He has got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth}  depending on the coffee."
+
+puts "If i add #{age}, #{height} and #{weight} i get #{age + height + weight}. "
